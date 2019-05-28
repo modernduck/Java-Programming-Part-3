188905f60b46efa76608680a70208195a695258d
modernduck <sompop.kulapalanont@gmail.com>
2019-05-28 11:29:43 +0700

    counter 3 & sum1

---
+++ HelloLoop/src/Counter3.java
@@ -0,0 +1,18 @@
+
+public class Counter3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.print(14 % 7);;
+		int counter = 1;
+		while(counter <= 30) {
+			if(counter % 7 == 0) {
+				System.out.print("HAHAHA ");
+			}else {
+				System.out.print(counter + " ");
+			}
+			counter ++;
+		}
+	}
+
+}
---
+++ HelloLoop/src/Sum1.java
@@ -0,0 +1,16 @@
+
+public class Sum1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		int sum = 0;
+		while(i <= 100) {
+			System.out.println("i " + i);
+			i = i + 1;
+			sum = sum + i;
+		}
+		System.out.println("SUM " + sum);
+	}
+
+}
