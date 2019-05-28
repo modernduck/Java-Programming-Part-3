1296a72130ba8d6ab3db78f23e2db204ffc296cd
modernduck <sompop.kulapalanont@gmail.com>
2019-05-28 13:39:59 +0700

    readfilemaxmin

--- FileReader/src/MyFileReader.java
+++ FileReader/src/MyFileReader.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.lang.Math;
 public class MyFileReader {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -9,13 +10,24 @@ public class MyFileReader {
 		Scanner sc = new Scanner(myfile);
 		float sum = 0;
 		int count = 0;
+		float max = -99999999;
+		float min = 999999999;
 		while(sc.hasNextFloat()) {
 			float b = sc.nextFloat();
+			if(count == 0 ) {
+				max = b;
+				min = b;
+			}else {
+				max = Math.max(max, b);
+				min = Math.min(min, b);
+			}
 			sum += b;
 			count = count + 1;
 		}
 		System.out.println("SUM " + sum);
 		System.out.println("AVG " + (sum / count));
+		System.out.println("Max " + max);
+		System.out.println("Min " + min);
 		
 		
 	}
