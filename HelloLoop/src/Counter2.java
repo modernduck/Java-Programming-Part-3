7d7107e1bdf9637e87707306dc80d2457928928b
modernduck <sompop.kulapalanont@gmail.com>
2019-05-28 11:10:24 +0700

    Counter 2

---
+++ HelloLoop/src/Counter2.java
@@ -0,0 +1,13 @@
+
+public class Counter2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter = 500;
+		while(counter >= 200) {
+			System.out.print(counter + " ");
+			counter = counter - 5;
+		}
+	}
+
+}
